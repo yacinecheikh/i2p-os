600eeb70fde3cf12f27b94afe4da144f17ab03b1
Yacine Cheikhrouhou <yacine.cheikhrouhou@yahoo.fr>
2024-12-11 03:23:20 +0100

    static ip configuration for the workstation

--- bootstrap-workstation.sh
+++ bootstrap-workstation.sh
@@ -3,7 +3,7 @@
 target="$1"
 
 useage() {
-	echo "Useage: ./bootstrap-router.sh <install directory>"
+	echo "Useage: ./bootstrap-workstation.sh <install directory>"
 }
 
 if [ -z "$target" ]
@@ -54,7 +54,7 @@ adduser --disabled-password --comment "" user
 passwd -d user
 adduser user sudo
 
-echo "i2p-os-router" > /etc/hostname
+echo "i2p-os-workstation" > /etc/hostname
 
 echo "fs	/       virtiofs        defaults        0       0" >> /etc/fstab
 END
@@ -62,21 +62,20 @@ END
 
 
 
-# install I2P
-LANG=C.UTF-8 arch-chroot $target /bin/bash <<END1
-apt install -y wget default-jdk
+## install I2P
+#LANG=C.UTF-8 arch-chroot $target /bin/bash <<END1
+#apt install -y wget default-jdk
+#
+## wget https://geti2p.net/en/download/2.7.0/clearnet/https/files.i2p-projekt.de/i2pinstall_2.7.0.jar/download -O i2pinstall_2.7.0.jar
+#
+## java -jar i2pinstall_2.7.0.jar -console
+#
+#sudo -u user bash <<END2
+#cd
+#wget https://files.i2p-projekt.de/2.7.0/i2pinstall_2.7.0.jar
+#echo -e "\n1\n1\n\nO\n1\n1\n" | java -jar i2pinstall_2.7.0.jar -console
+#END2
+#END1
 
-# wget https://geti2p.net/en/download/2.7.0/clearnet/https/files.i2p-projekt.de/i2pinstall_2.7.0.jar/download -O i2pinstall_2.7.0.jar
-
-# java -jar i2pinstall_2.7.0.jar -console
-
-sudo -u user bash <<END2
-cd
-wget https://files.i2p-projekt.de/2.7.0/i2pinstall_2.7.0.jar
-echo -e "\n1\n1\n\nO\n1\n1\n" | java -jar i2pinstall_2.7.0.jar -console
-END2
-END1
-
-
-# static IP address in the isolated network
-cp "resources/router/Wired connection 2.nmconnection" "$target/etc/NetworkManager/system-connections/"
+# static IP address configuration
+cp "resources/workstation/Wired connection 1.nmconnection" "$target/etc/NetworkManager/system-connections/"
